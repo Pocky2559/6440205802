26d784c1bc3404eb918f316a7a097a30c1dc4230
Pocky2559 <makkop587@gmail.com>
2024-01-22 22:16:37 +0700

    Update UpgradeStatus.cs
    Edit Lumber Camp Upgrading

--- Assets/Scripts/Upgrade/UpgradeStatus.cs
+++ Assets/Scripts/Upgrade/UpgradeStatus.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeStatus : MonoBehaviour //this script use to tell what upgrade you have already done.
 {
@@ -12,4 +13,78 @@ public class UpgradeStatus : MonoBehaviour //this script use to tell what upgrad
 
     public bool isFoodGatheringSpeedUpgrade;
     public bool isFoodGatheringCapacityUpgrade;
+
+    public Button woodGatheringSpeedButton;
+    public Button woodGatheringCapacityButton;
+    public Button goldAndStoneGatheringSpeedButton;
+    public Button goldAndStoneGatheringCapacityButton;
+    public Button foodGatheringSpeedButton;
+    public Button foodGatheringCapacityButton;
+
+    public ResourcesStatus resourcesStatus;
+    public EconomicUpgradeDatabase economicUpgradeDatabase;
+
+    public void UpgradeWoodGatheringSpeed() // Click upgrade wood gathering speed
+    {
+        if(resourcesStatus.gold_Amount >= economicUpgradeDatabase.economicUpgrade[0].goldCostForUpgradeGatheringSpeed)
+        {
+            if(isWoodGatheringSpeedUpgrade == false)
+            {
+                isWoodGatheringSpeedUpgrade = true;
+                resourcesStatus.gold_Amount = resourcesStatus.gold_Amount - economicUpgradeDatabase.economicUpgrade[0].goldCostForUpgradeGatheringSpeed;
+                woodGatheringSpeedButton.enabled = false;
+            }
+            else
+            {
+                Debug.Log("You have already upgrade");
+            }
+        }
+        
+        if(resourcesStatus.gold_Amount < economicUpgradeDatabase.economicUpgrade[0].goldCostForUpgradeGatheringSpeed)
+        {
+            Debug.Log("Not Enough Gold");
+        }
+    }
+
+    public void UpgradeWoodGatheringCapacity() // Click upgrade wood gathering capacity
+    {
+        if(resourcesStatus.gold_Amount >= economicUpgradeDatabase.economicUpgrade[0].goldCostForUpgradeGatheringCapacity)
+        {
+            if (isWoodGatheringCapacityUpgrade == false)
+            {
+                isWoodGatheringCapacityUpgrade = true;
+                resourcesStatus.gold_Amount = resourcesStatus.gold_Amount - economicUpgradeDatabase.economicUpgrade[0].goldCostForUpgradeGatheringSpeed;
+                woodGatheringCapacityButton.enabled = false;
+            }
+            else
+            {
+                Debug.Log("You have already upgrade");
+            }
+        }
+
+        if (resourcesStatus.gold_Amount < economicUpgradeDatabase.economicUpgrade[0].goldCostForUpgradeGatheringCapacity)
+        {
+            Debug.Log("Not Enough Gold");
+        }
+    }
+
+    public void UpgradeGoldStoneGatheringSpeed() // Click upgrade gold stone gathering speed
+    {
+        isGoldStoneGatheringSpeedUpgrade = true;
+    }
+
+    public void UpgradeGoldStoneGatheringCapacity() // Click upgrade gold stone gathering capacity
+    {
+        isGoldStoneGatheringCapacityUpgrade = true;
+    }
+
+    public void UpgradeFoodGatheringSpeed() // Click upgrade food gathering speed
+    {
+        isFoodGatheringSpeedUpgrade = true;
+    }
+
+    public void UpgradeFoodGatheringCapacity() // Click upgrad food gathering capacity
+    {
+        isFoodGatheringCapacityUpgrade = true;
+    }
 }
